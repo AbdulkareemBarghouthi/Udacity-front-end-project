00d0e1de872f329a0e0abecc431e693a56cf5cf5
Abdulkareem <k.bar123@hotmail.com>
2018-12-10 05:25:19 +0200

    Added layout

---
+++ js/Model/locations.js
@@ -0,0 +1,15 @@
+var locations = [
+    { "title": "Alia Restaurant", "location": { lat: 31.949213, lng: 35.892162 } },
+    { "title": "Byzantine Oil Press", "location": { lat: 31.944351, lng: 35.891111 } },
+    { "title": "Orthodox Sports Club", "location": { lat: 31.947592, lng: 35.896475 } },
+    { "title": "Starbucks Coffee", "location": { lat: 31.942221, lng: 35.889888 } },
+    { "title": "Embassy of the United States", "location": { lat: 31.945098, lng: 35.880532 } },
+    { "title": "Al Khalidi Hospital and Medical Center", "location": { lat: 31.950487, lng: 35.903878 } },
+    { "title": "Taj Mall", "location": { lat: 31.941359, lng: 35.888048 } },
+    { "title": "Blue Fig", "location": { lat: 31.941622, lng: 35.889700 } },
+    { "title": "Haya Cultural Center", "location": { lat: 31.966289, lng: 35.896711 } },
+    { "title": "Abdali Boulevard", "location": { lat: 31.964542, lng: 35.905209 } },
+    { "title": "Jordan National Gallery of Arts", "location": { lat: 31.957915, lng: 35.915251 } },
+    { "title": "Amman Citadel", "location": { lat: 31.954347, lng: 35.935421 } },
+    { "title": "Roman Theatre", "location": { lat: 31.951665, lng: 35.939358 } }
+]
--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
+var Controller = {
+    getAllLocations: function() {
+        return locations;
+    }
+}
+
+
+
+
+// here's where i load the map
 var map;
+var allMarkers = [];
 
 function initMap() {
-    try {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 31.945367,
-                lng: 35.928372
-            },
-            zoom: 13
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 31.945367,
+            lng: 35.928372
+        },
+        zoom: 13
+    });
+    var markers = Controller.getAllLocations();
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: "Hey there"
+    });
+
+    for (var i = 0; i < markers.length; i++) {
+        var marker = new google.maps.Marker({
+            position: markers[i].location,
+            title: markers[i].title,
+            map: map
         });
-    } catch (err) {
-        alert("Something went wrong try again later" + err.message);
+        allMarkers.push(marker);
+
+        marker.addListener('click', function() {
+            // initializeInfoWindows(this, infoWindow);
+            infoWindow.open(map, this);
+        });
+
     }
+
 }
 
 // as the google documentation states this function handles any authentication failures with api key
@@ -19,3 +47,34 @@ function gm_authFailure() {
     var ErrorDiv = document.createElement('div');
     alert("API authentication error, We promise we'll fix this as soon as we can :)");
 }
+
+// Populate infoWindow
+function initializeInfoWindows(marker, infoWindow) {
+    infoWindow.marker = marker;
+    // infoWindow.setContent(marker.title);
+    infoWindow.setContent('<div>' + marker.title + '</div>');
+    infoWindow.open(map, marker);
+}
+
+var ViewModel = function() {
+    var self = this;
+    self.userInput = ko.observable("");
+    self.listLocations = ko.observableArray([]);
+    locations.forEach(function(item) {
+        self.listLocations.push(item);
+    });
+    self.resultLocations = ko.observableArray([]);
+
+    self.filterFunction = function() {
+        for (var i = 0; i <= locations.length; i++) {
+            this.input = self.userInput.toString().toLowerCase();
+            this.location = self.locations[i].title;
+            alert("Location: " + this.location + " Input: " + this.input);
+            if (this.location.includes(this.input)) {
+                self.resultLocations.push(listLocations[i].title);
+            }
+        }
+    }
+
+}
+ko.applyBindings(new ViewModel);

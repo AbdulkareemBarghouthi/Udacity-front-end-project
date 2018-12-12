fc6f4b5446ce056edef9a5c74ed6470a5001d9a9
Abdulkareem <k.bar123@hotmail.com>
2018-12-12 07:58:41 +0200

    finished infowindow

--- js/app.js
+++ js/app.js
@@ -1,75 +1,9 @@
 // Here's an object that provides helper functions acting closely like a controller but i like to consider it as a middleware
+'use strict';
 var helperObject = {
     articleList: [],
     getAllLocations: function() {
         return locations;
-    },
-    getWikiArticles: function(title) {
-        $.ajax({
-            url: 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + title + '&format=json&callback=wikiCallback',
-            dataType: "jsonp",
-            success: function(response) {
-                var cardList = document.getElementById("wikiList");
-                var card = document.getElementById("extraInfo");
-                while (cardList.firstChild) {
-                    cardList.removeChild(cardList.firstChild);
-                }
-                var article = document.createElement("li");
-                if (response[2].length == 0 || response[2] == "") {
-                    article.innerHTML = "No wikipedia links available for this restaraunt :/";
-                    cardList.appendChild(article);
-                    return false;
-                };
-
-                article.innerHTML = "<a href=http://en.wikipedia.org/wiki/" + response[0] + ">" + response[0] + "</a>";
-
-                cardList.appendChild(article);
-                return response;
-            }
-        }).fail(function() {
-            alert("Something went wrong try again later :/");
-        });
-    },
-    getFourSquareInfo: function(data) {
-        var self = this;
-        $.ajax({
-            client_id: "0QVUHV412GGI1M4DCDYHSXK4LCYVASHMILEBMBR000RZ4TSR",
-            client_secret: "GT3HKHHLLUP3R5JQQLUNF1R2YLBU0JLCEHUFWN4IHGL01AGI",
-            url: "https://api.foursquare.com/v2/venues/search?client_id=0QVUHV412GGI1M4DCDYHSXK4LCYVASHMILEBMBR000RZ4TSR&client_secret=GT3HKHHLLUP3R5JQQLUNF1R2YLBU0JLCEHUFWN4IHGL01AGI&v=20180323&limit=1&ll=" + data.location.lat + "," + data.location.lng + "&query=" + data.title.replace(" ", "%20"),
-            dataType: "jsonp",
-            method: "GET",
-            success: function(response) {
-                var fsList = document.getElementById("fourSquareList");
-                var container = document.getElementById("extraInfo");
-                while (fsList.firstChild) {
-                    fsList.removeChild(fsList.firstChild);
-                }
-                if (response.length == 0) {
-                    var error = document.createElement("p");
-                    error.innerHTML = "No forsquare articles available :/"
-                    fsList.appendChild(error);
-                }
-                container.removeChild(container.lastChild);
-                var address = response["response"].venues[0].location.formattedAddress[0];
-                var category = response["response"].venues[0].categories[0].name;
-                var iconSetup = response["response"].venues[0].categories[0].icon;
-                var icon = iconSetup.prefix + "64" + iconSetup.suffix;
-                var city = response["response"].venues[0].location.formattedAddress[1];
-
-                var fsItem = document.createElement("li");
-                var fscategoryItem = document.createElement("li");
-                var fsIcon = document.createElement("img");
-
-                fscategoryItem.innerHTML = category;
-                fsItem.innerHTML = address;
-                fsIcon.setAttribute("src", icon);
-                fsList.appendChild(fsItem);
-                fsList.appendChild(fscategoryItem);
-                container.appendChild(fsIcon);
-            }
-        }).fail(function() {
-            alert("Something went wrong try again later :/");
-        });
     },
     getDirections: function(data) {
         var self = this;
@@ -90,13 +24,12 @@ var helperObject = {
                         directions: response
                     });
                     success = response["routes"][0]["legs"];
-                    console.dir(success);
                     var infoWindow = new google.maps.InfoWindow();
-                    var infoWindowContent = "<h5>here is the route to your location</h5><br>" +
+                    console.dir(success);
+                    return "<h5>here is the route to your location</h5><br>" +
                         "Restaurant is this far away: " + success[0]["distance"].text + " And it will approximately take: " +
                         success[0]["duration"].text;
-                    infoWindow.setContent(infoWindowContent);
-                    infoWindow.open(map, data);
+
                 }
             }
         )
@@ -104,14 +37,11 @@ var helperObject = {
 }
 
 
-
-
 // here's where i load the map
 var map;
 var allMarkers = [];
 
 function initMap() {
-    allMarkers = [];
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 26.13804,
@@ -130,14 +60,47 @@ function initMap() {
             map: map
         });
         allMarkers.push(marker);
-
+        var infowindow = new google.maps.InfoWindow();
         marker.addListener('click', function() {
-            // helperObject.getDirections(this)
+            populateInfoWindow(this, infowindow)
+            infowindow.open(map, this);
         });
-
     }
 
 }
+
+function populateInfoWindow(marker, infowindow) {
+    var self = this;
+    var markerPosition = marker.getPosition();
+    var myUrl = "https://api.foursquare.com/v2/venues/search?client_id=0QVUHV412GGI1M4DCDYHSXK4LCYVASHMILEBMBR000RZ4TSR&client_secret=GT3HKHHLLUP3R5JQQLUNF1R2YLBU0JLCEHUFWN4IHGL01AGI&v=20180323&limit=1&ll=" + markerPosition.lat().toString() + "," + markerPosition.lng().toString() + "&query=" + marker.title.split(" ").join("%20");
+    console.log(myUrl);
+    console.log(marker.title.toString());
+    $.ajax({
+        client_id: "0QVUHV412GGI1M4DCDYHSXK4LCYVASHMILEBMBR000RZ4TSR",
+        client_secret: "GT3HKHHLLUP3R5JQQLUNF1R2YLBU0JLCEHUFWN4IHGL01AGI",
+        url: myUrl,
+        dataType: "jsonp",
+        method: "GET",
+        success: function(response) {
+            if (!response["response"].venues[0]) {
+                infowindow.setContent("We couldn't fetch any foursquare data for this location :/");
+                return;
+            }
+            var address = response["response"].venues[0].location.formattedAddress[0];
+            var category = response["response"].venues[0].categories[0].name;
+            var iconSetup = response["response"].venues[0].categories[0].icon;
+            var icon = iconSetup.prefix + "64" + iconSetup.suffix;
+            var city = response["response"].venues[0].location.formattedAddress[1];
+            var result = { address: address, name: category, icon: icon, city: city };
+            var content = "<h4>" + category + "</h4>" +
+                "<p>address: " + address + "</p>" + "<p>city: " + city + "</p>";
+            infowindow.setContent(content);
+        }
+    }).fail(function() {
+        alert("Something was wrong while we were dealing with the foursquare API");
+    });
+
+};
 
 // as the google documentation states this function handles any authentication failures with api key
 function gm_authFailure() {
@@ -149,16 +112,18 @@ function gm_authFailure() {
 // ViewModel for knockoutjs
 var ViewModel = function() {
     var self = this;
-
+    // Manipulate DOM through these observables
     self.checkVisibilty = ko.observable(false);
     self.wikiResult = ko.observableArray([]);
     self.fourSquareResult = ko.observable();
+
     // Storing the full location of the site
     self.userInput = ko.observable("");
     self.listLocations = ko.observableArray([]);
     locations.forEach(function(item) {
         self.listLocations.push(item);
     });
+
 
     self.resultLocations = ko.observableArray(self.listLocations());
     self.result = ko.observable();
@@ -229,7 +194,7 @@ var ViewModel = function() {
                 self.fourSquareResult(result);
             }
         }).fail(function() {
-            alert("someshit");
+            alert("Something was wrong while we were dealing with the foursquare API");
         });
     }
 
@@ -242,32 +207,18 @@ var ViewModel = function() {
         allMarkers.forEach(function(item) {
             if (item.title == data.title) {
                 self.marker = item;
-                var panorama = new google.maps.StreetViewPanorama(
-                    document.getElementById('pano'), {
-                        position: item.position,
-                        pov: {
-                            heading: 10,
-                            pitch: 5
-                        }
-                    });
-                map.setStreetView(panorama);
-                self.marker.addListener("click", function() {
-                    self.checkVisibilty(true);
-                    self.getWikiLinks(data);
-                    self.getFourSquareInfo(data);
-                });
             }
             item.setAnimation(null);
         });
         self.marker.setAnimation(google.maps.Animation.BOUNCE);
 
-        // Get directions API
-        helperObject.getDirections(data);
     }
 
     self.hideElement = function() {
         self.checkVisibilty(false);
     }
 
+
 }
+
 ko.applyBindings(new ViewModel());
